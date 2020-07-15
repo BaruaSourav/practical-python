2e431b3c81f14230691f3d1067a4b6ab64349420
Sourav Barua <sourav.shr6@gmail.com>
2020-07-15 20:51:03 -0230

    Worked on exercise 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
-#
-# Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+first_year_payment = 1000.0
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if month >= 1 and month <= 12:
+        principal = principal - first_year_payment
+        total_paid = total_paid + first_year_payment
+
+    print("Month "+ str(month))
+    print("Total Paid : " + str(total_paid))
+    print("Principal : " + str(principal))
+    print("--------------------------------")
+    
+print('Total paid :' + str(round(total_paid, 2)))
+print('Months :' + str(month))
+
+
+
