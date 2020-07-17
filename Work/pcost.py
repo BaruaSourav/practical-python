606bdb14a57703cb2b1457d309fde6a697b2da77
Sourav Barua <sourav.shr6@gmail.com>
2020-07-16 22:43:03 -0230

    Exercise 1.33 solved.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,8 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
 def portfolio_cost(filename):
     totalCost = 0.0
 
@@ -11,8 +13,16 @@ def portfolio_cost(filename):
                 row = line.split(',')
                 cost = int(row[1])*float(row[2])
                 totalCost += cost
-            print(f'Total Cost ${totalCost}')
+               
             except ValueError:
                 print('Value error occured')
-cost = portfolio_cost('Data/portfolio.csv')
-print('Total cost:', cost)
+        
+    return totalCost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(f'Total Cost ${cost}')
