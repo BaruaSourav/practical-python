1dbe2c3abb24ef6800ccf32794eed9ec1e411f8c
Sourav Barua <sourav.shr6@gmail.com>
2020-07-21 21:03:30 -0230

    Worked on exercises 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+import csv
+
+def read_portfolio(file):
+    portfolio=[]
+    with open(file) as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for item in rows:
+            holding = (item[0],int(item[1]),float(item[2]))
+            portfolio.append(holding)
+    return portfolio
+
+            
